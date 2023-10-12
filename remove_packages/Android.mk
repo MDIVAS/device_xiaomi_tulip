5d7a6a04ccc33c52cc5994cfd7615dd640517598
Le Hong Duc <hongducthbk123@gmail.com>
2023-10-12 23:00:40 +0700

    tulip: remove_packages: Restore some packages We now have retrofitted partition, let's restore some Google bloatware

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,32 +5,22 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    AmbientSensePrebuilt \
     AppDirectedSMSService \
     arcore \
     BetterBugStub \
-    CbrsNetworkMonitor \
     ConnMO \
     DCMO \
     DevicePolicyPrebuilt \
-    DiagnosticsToolPrebuilt \
     DMService \
     Drive \
-    GCS \
-    GoogleTTS \
     MaestroPrebuilt \
     Maps \
-    Photos \
-    PixelLiveWallpaperPrebuilt \
-    PixelWallpapers2022 \
     PlayAutoInstallConfig \
     PrebuiltGmail \
-    RecorderPrebuilt \
     SCONE \
     ScribePrebuilt \
     Showcase \
     SoundAmplifierPrebuilt \
-    SprintHM \
     talkback \
     Tycho \
     VZWAPNLib \
